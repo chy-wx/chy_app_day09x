edccd980e36cd588a850ea7a9113dc5b4c826d8c
za-wangxu <wangxu@zhongan.io>
2019-05-31 09:48:48 +0800

    删除脚本002

--- scripts/test_002.py
+++
@@ -1,8 +0,0 @@
-import allure
-
-
-class Test_002:
-
-    def test002(self):
-        with open(r"C:\Users\James\Desktop\app8\scripts\chy.png", "rb") as f:
-            allure.attach("图片", f.read(), allure.attach_type.PNG)
